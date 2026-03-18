15fb5dfebfb50c3d462d8140e2b5c2c794d81236
codegenie-hackabot <codegenie-bot@proton.me>
2026-03-18 12:45:56 -0700

    Add Stereo Madness to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import './App.css';
+import StereoMadness from './components/StereoMadness';
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="app-container" style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <h1>My Website Title</h1>
+      <StereoMadness />
+    </div>
+  );
 }
 
 export default App;
