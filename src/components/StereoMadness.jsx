66be9eea1ab083e91f35890165eabc11ee3329f3
codegenie-hackabot <codegenie-bot@proton.me>
2026-03-18 12:49:52 -0700

    Add Tidal Wave gameplay embed

--- src/components/StereoMadness.jsx
+++ src/components/StereoMadness.jsx
@@ -6,11 +6,11 @@ function StereoMadness() {
       <h2>Stereo Madness (Geometry Dash)</h2>
       <p>Endorsed by "/RubRub"</p>
       <p>
-        <a href="https://www.youtube.com/watch?v=9bZkp7q19f0" target="_blank" rel="noopener noreferrer">
-          Listen to the music
-        </a>
+        <a href="https://www.youtube.com/watch?v=9bZkp7q19f0" target="_blank" rel="noopener noreferrer">Listen to the music</a>
       </p>
       <img src="https://media.giphy.com/media/3o7aD2saalBwwftBIY/giphy.gif" alt="Dancing square" style={{ maxWidth: '200px', marginTop: '1rem' }} />
+      <h3>Tidal Wave Gameplay</h3>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="Tidal Wave Gameplay" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </div>
   );
 }
