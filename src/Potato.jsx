cb4a49525889b79096f2e5acc661a9a7adeefe2b
codegenie-hackabot <codegenie-bot@proton.me>
2026-03-17 19:28:36 -0700

    Add Potato component

---
+++ src/Potato.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Potato.css';
+
+const Potato = () => (
+  <div className="potato-container">
+    <img src="https://via.placeholder.com/100?text=Potato" alt="Spinning Potato" className="potato-image" />
+  </div>
+);
+
+export default Potato;
