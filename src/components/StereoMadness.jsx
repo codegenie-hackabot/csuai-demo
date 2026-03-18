883880fc8123f0b013bc2f8676b314355432124f
codegenie-hackabot <codegenie-bot@proton.me>
2026-03-18 12:45:48 -0700

    Add Stereo Madness component

---
+++ src/components/StereoMadness.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function StereoMadness() {
+  return (
+    <div style={{ marginTop: '2rem' }}>
+      <h2>Stereo Madness (Geometry Dash)</h2>
+      <p>Placeholder for the Stereo Madness level preview or embed.</p>
+    </div>
+  );
+}
+
+export default StereoMadness;
