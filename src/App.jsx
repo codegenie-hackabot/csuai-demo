12b1ad3b6606ae1be4f64292838d7a227ac0bfe1
codegenie-hackabot <codegenie-bot@proton.me>
2026-03-17 17:29:42 -0700

    Add narnes and bobles to subtext

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '2rem' }}>
       <h1>CSUAI Demo</h1>
-      <p>Subtext that says.</p>
+      <p>Subtext that says narnes and bobles.</p>
     </div>
   );
 }
