f7323e2d2ab16255ff4d216a49438d047bb330ff
codegenie-hackabot <codegenie-bot@proton.me>
2026-03-18 16:58:34 -0700

    Simplify App.jsx content

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,10 @@
 import React from 'react';
-import './App.css';
-import BouncingBall from './BouncingBall';
-import CameraFeed from './CameraFeed';
-import ContactModal from './ContactModal';
-import DinoGame from './DinoGame';
-import Potato from './Potato';
-import SlotMachine from './SlotMachine';
-import Stopwatch from './Stopwatch';
-import VoiceRecorder from './VoiceRecorder';
-import StereoMadness from './components/StereoMadness';
-import GTA6 from './components/GTA6';
-import PollMock from './PollMock';
 
 function App() {
   return (
-    <div className="app-container" style={{ textAlign: 'center', marginTop: '2rem' }}>
-      <h1>My Website Title 🏴‍☠️</h1>
-      <div>
-        <h2>Kirby and Jigglypuff 💀</h2>
-        <img src="https://static.imgburr.com/static/kirby.png" width="100" height="100" alt="Kirby" />
-        <img src="https://static.imgburr.com/static/jigglypuff.png" width="100" height="100" alt="Jigglypuff" />
-      </div>
-      {/* Render all demo components */}
-      <StereoMadness />
-      <GTA6 />
-      <BouncingBall />
-      <CameraFeed />
-      <ContactModal />
-      <DinoGame />
-      <Potato />
-      <SlotMachine />
-      <Stopwatch />
-      <VoiceRecorder />
-      <PollMock />
+    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <h1>Welcome to the Fun website!</h1>
+      <p>This is a simplified version of the app.</p>
     </div>
   );
 }
