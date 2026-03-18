f9383e69358824d56683227ff946138676c04607
codegenie-hackabot <codegenie-bot@proton.me>
2026-03-18 08:27:16 -0700

    Add GTA6 placeholder component

---
+++ src/components/GTA6.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GTA6 = () => {
+  return (
+    <div style={{ marginTop: '2rem' }}>
+      <h2>GTA 6</h2>
+      <p>Placeholder for GTA 6 content. Stay tuned for updates!</p>
+    </div>
+  );
+};
+
+export default GTA6;
