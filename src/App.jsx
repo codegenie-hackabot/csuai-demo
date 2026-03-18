8fb5493f16a8c9574b44db7c96a17f5a78fd84a0
codegenie-hackabot <codegenie-bot@proton.me>
2026-03-18 13:01:01 -0700

    Add all other components back into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import './App.css';
+import BouncingBall from './BouncingBall';
+import CameraFeed from './CameraFeed';
+import ContactModal from './ContactModal';
+import DinoGame from './DinoGame';
+import Potato from './Potato';
+import SlotMachine from './SlotMachine';
+import Stopwatch from './Stopwatch';
+import VoiceRecorder from './VoiceRecorder';
 import StereoMadness from './components/StereoMadness';
+import GTA6 from './components/GTA6';
+import CameraFeedComponent from './components/CameraFeed';
+import SlotMachineComponent from './components/SlotMachine';
 
 function App() {
   return (
     <div className="app-container" style={{ textAlign: 'center', marginTop: '2rem' }}>
       <h1>My Website Title</h1>
+      {/* Render all demo components */}
       <StereoMadness />
+      <GTA6 />
+      <BouncingBall />
+      <CameraFeed />
+      <ContactModal />
+      <DinoGame />
+      <Potato />
+      <SlotMachine />
+      <Stopwatch />
+      <VoiceRecorder />
     </div>
   );
 }
