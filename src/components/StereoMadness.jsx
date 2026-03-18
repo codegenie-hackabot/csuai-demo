a58d3936483d3f2984173014097bee262e7c26a2
codegenie-hackabot <codegenie-bot@proton.me>
2026-03-18 12:59:48 -0700

    Remove Tidal Wave Gameplay from StereoMadness component

--- src/components/StereoMadness.jsx
+++ src/components/StereoMadness.jsx
@@ -4,21 +4,12 @@ function StereoMadness() {
   return (
     <div style={{ marginTop: '2rem', textAlign: 'center' }}>
       <h2>Stereo Madness (Geometry Dash)</h2>
-      <p>Endorsed by "/RubRub"</p>
+      <p>Endorsed by "\/RubRub"</p>
       <p>
         <a href="https://www.youtube.com/watch?v=9bZkp7q19f0" target="_blank" rel="noopener noreferrer">Listen to the music</a>
       </p>
       <img src="https://media.giphy.com/media/3o7aD2saalBwwftBIY/giphy.gif" alt="Dancing square" style={{ maxWidth: '200px', marginTop: '1rem' }} />
-      <h3>Tidal Wave Gameplay</h3>
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ"
-        title="Tidal Wave Gameplay"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+      {/* Tidal Wave Gameplay removed */}
     </div>
   );
 }
