75d2978af0002daec1e81aca43f3e7d574bdef9d
codegenie-hackabot <codegenie-bot@proton.me>
2026-03-19 12:51:20 -0700

    Add Header to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import SlotMachine from './SlotMachine';
 import ChessGame from './ChessGame';
+import Header from './Header';
 
 function App() {
   return (
-    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
-      <h1>Welcome to the Skibidi website!</h1>
-      <SlotMachine />
-      <ChessGame />
-      <p>This is a simplified version of the app.</p>
+    <div>
+      <Header />
+      <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+        <h1>Welcome to the Skibidi website!</h1>
+        <SlotMachine />
+        <ChessGame />
+        <p>This is a simplified version of the app.</p>
+      </div>
     </div>
   );
 }
