56174aa67cd21ed18ae22df4dcf7110d0c4eeac7
codegenie-hackabot <codegenie-bot@proton.me>
2026-03-19 12:49:54 -0700

    Add ChessGame to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import SlotMachine from './SlotMachine';
+import ChessGame from './ChessGame';
 
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '2rem' }}>
       <h1>Welcome to the Skibidi website!</h1>
       <SlotMachine />
+      <ChessGame />
       <p>This is a simplified version of the app.</p>
     </div>
   );
